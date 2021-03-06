80d801d5486532359993c031e7e24c67c5272616
daniel <adeboyed93@gmail.com>
2021-03-06 18:25:50 +0100

    migrations msg

--- savers/models.py
+++ savers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Table(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False , blank=True)
+    app_name = models.CharField(max_length=30, blank=True, null=True) 
+    Email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=12,null=True,blank=True)
+    username = models.CharField(max_length=100, blank=True, null=True)
+    password = models.CharField(max_length=100 , blank=False, null=False)
+
+    def __str__(self):
+        return self.app_name
+
